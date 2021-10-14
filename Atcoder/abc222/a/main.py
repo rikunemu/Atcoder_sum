af85ed38502a17947c048e91319a106b27d9c789
rikunemu <50669045+rikunemu@users.noreply.github.com>
2021-10-14 18:54:13 +0900

    Atcoder再開

---
+++ Atcoder/abc222/a/main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+from itertools import accumulate,chain,combinations,groupby,permutations,product
+from collections import deque,Counter
+from bisect import bisect_left,bisect_right
+from math import gcd,sqrt,sin,cos,tan,degrees,radians,ceil,floor
+from fractions import Fraction
+from decimal import Decimal
+import sys
+#rstripが必要なことも
+#input = lambda: sys.stdin.readline().rstrip()
+#inputの高速化、基本はいらん、入力が長いときに使用
+#from sys import setrecursionlimit
+#setrecursionlimit(10**7)
+MOD=10**9+7
+INF=float('inf')
+#float型の無限大inf
+def resolve():
+    n=(input())
+    #a, b = map(int, input().split())
+    #A = list(map(int, input().split()))
+    if len(n)==4:
+        print(n)
+    else:
+        print("".join(list(["0"]*(4-len(n))+list(n))))
+
+
+if __name__ == "__main__":
+    resolve()
+
+
