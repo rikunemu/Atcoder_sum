5e769e5c3a87f7539a91c219e165b71975f2a6d8
rikunemu <rikutotennis1511@icloud.com>
2024-08-18 17:47:00 +0900

    abc367 b問題

---
+++ Atcoder/abc367/README.md
@@ -0,0 +1,15 @@
+## a問題
+
+### 自分の解き方
+
+if分で24時をまたぐかまたがないかで条件分岐をした
+
+## b問題
+
+### 自分の解き方
+
+[【Python】末尾の不要な0を削除する方法](https://ishi-tech.biz/python-decimal-normalize/)を参考に
+
+- isinstance関数: 調べたい型が指定した型やデータに属しているかチェックする関数
+
+- float.is_integer関数:float型について値が整数であれば、True,そうでなければFalseを返す
--- Atcoder/abc367/b/main.py
+++ Atcoder/abc367/b/main.py
@@ -14,10 +14,31 @@ import sys
 MOD=10**9+7
 INF=float('inf')
 #float型の無限大inf
+
+def decimal_normalize(value):
+    """
+    浮動小数点数の場合、末尾の不要な0を削除する。
+    整数値の場合、そのまま整数として返す。
+
+    Parameters
+    ----------
+    value : float or int
+        対象となる数値。
+
+    Returns
+    -------
+    int or float
+        不要な末尾の0が削除された数値。
+    """
+    if isinstance(value, float) and value.is_integer():
+        return int(value)
+    return value
+
 def resolve():
-    n=int(input())
+    x=float(input())
     #a, b = map(int, input().split())
     #A = list(map(int, input().split()))
+    print(decimal_normalize(x))
 
 if __name__ == "__main__":
     resolve()
